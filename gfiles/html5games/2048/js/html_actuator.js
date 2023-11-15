68cd5e5e8d114c820b685903f3cb0d266e510d17
TheCodingGroup <ribbon4me@gmail.com>
2023-11-15 14:26:01 -0600

    Update html_actuator.js

--- gfiles/html5games/2048/js/html_actuator.js
+++ gfiles/html5games/2048/js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "GAME Over.";
+  var message = won ? "Game Won." : "Game Over.";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
